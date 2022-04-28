e4904909310974fc6860bcb3f7fbe78879b868d7
SuneelM <python.linux789@gmail.com>
2022-04-28 18:26:09 -0400

    Add Build

---
+++ build/precache-manifest.123f2cef8ce94109282ed63a453f6db0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7704378efc50271ff97a",
+    "url": "/static/js/main.7704378e.chunk.js"
+  },
+  {
+    "revision": "756c97202389e89aa903",
+    "url": "/static/js/1.756c9720.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7d024aee40f9b53dfb48452c517fbad8",
+    "url": "/index.html"
+  }
+];
