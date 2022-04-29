5425f82e07f40bdb5da39d5229b732be3dacb5fe
SuneelM <python.linux789@gmail.com>
2022-04-28 21:32:56 -0400

    make it better

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify, request, json
 from flask_sqlalchemy import SQLAlchemy
-import os
 
 app = Flask(__name__, static_folder='./build', static_url_path='/')
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///notes.db"
