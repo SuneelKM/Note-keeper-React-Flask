0cb011a2e2603e63800ca830a908f6513b889e72
SuneelM <python.linux789@gmail.com>
2022-04-28 20:40:45 -0400

    update main.py to include / route

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__, static_folder='./build', static_url_path='/')
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL1", "sqlite:///notes.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
