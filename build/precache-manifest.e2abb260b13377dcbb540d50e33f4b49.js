291bc3dc47b829cf257dbd0d7d02bc021d88a9a3
SuneelM <python.linux789@gmail.com>
2022-04-30 02:22:00 -0400

    Update to delete database every 10 min

---
+++ build/precache-manifest.e2abb260b13377dcbb540d50e33f4b49.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "99f139cc24d9ec554249",
+    "url": "/static/js/main.99f139cc.chunk.js"
+  },
+  {
+    "revision": "04664813c7aa0f3f5519",
+    "url": "/static/js/1.04664813.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "122fac1a143b17e4a7a6445de0d6e22b",
+    "url": "/index.html"
+  }
+];
