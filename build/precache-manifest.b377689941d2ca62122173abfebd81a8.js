8192ffac6aecbac8bbd1bdd6e8cdf64d7f4f287a
SuneelKM <101069729+SuneelKM@users.noreply.github.com>
2022-04-30 03:32:53 -0400

    Add files via upload

---
+++ build/precache-manifest.b377689941d2ca62122173abfebd81a8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f6b2f9c5ab6f68f73a6",
+    "url": "/static/js/main.2f6b2f9c.chunk.js"
+  },
+  {
+    "revision": "04664813c7aa0f3f5519",
+    "url": "/static/js/1.04664813.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ec98b298cbf641315ab8538125466d09",
+    "url": "/index.html"
+  }
+];
